dbc2ab9d33529ac8f0dbd7644cf79c65f1eba007
abhishek-akp <abhishekkumarpandey2803@gmail.com>
2025-07-09 15:18:50 -0400

    ui mb

--- src/App.js
+++ src/App.js
@@ -18,7 +18,7 @@ const HeroSection = () => {
       }}
     >
       <div className="max-w-xl rounded">
-        <h2 className="text-left md:text-xl text-md font-bold text-black mb-4">
+        <h2 className="text-left md:text-xl text-sm font-bold text-black mb-4">
           Quality cleaning at a fair price.
         </h2>
         <h1 className="text-left md:text-5xl text-xl font-bold text-black leading-tight mb-6">
@@ -26,17 +26,17 @@ const HeroSection = () => {
           <br /> and thorough cleaning <br />
           services
         </h1>
-        <p className="md:text-xl text-md text-left text-gray-600 mb-8">
+        <p className="md:text-xl text-sm text-left text-gray-600 mb-8">
           We provide performing cleaning tasks using the least <br />
           amount of time, energy, and money.
         </p>
         <div className="flex gap-4 md:text-xl text-md">
-          <button className="bg-[#44acfc] text-white px-6 py-2 rounded shadow hover:bg-blue-500">
+          <button className="bg-[#44acfc] text-white px-6 py-2 rounded shadow hover:bg-blue-500 md:text-xl text-sm">
             Get Start Now
           </button>
           <button
             variant="outline"
-            className="border border-[#44acfc] text-[#44acfc] px-6 py-2 rounded hover:bg-[#f0faff]"
+            className="border border-[#44acfc] text-[#44acfc] px-6 py-2 rounded hover:bg-[#f0faff] md:text-xl text-sm"
           >
             View all Services
           </button>
@@ -49,7 +49,7 @@ const HeroSection = () => {
 const ServicesSection = () => {
   return (
     <section className="md:p-10 xl:p-35 lg:p-20 p-5 bg-white">
-      <div className="flex justify-between items-start mb-10">
+      <div className="flex md:flex-row flex-col justify-between items-start mb-10">
         <div>
           <h2 className="text-left text-3xl font-bold text-black mb-2">
             We Always Provide The <br />
