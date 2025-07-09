23ee2e1839956ae0e72f6ea81a966f9cc6694fc6
abhishek-akp <abhishekkumarpandey2803@gmail.com>
2025-07-09 15:10:32 -0400

    ui mb

--- src/App.js
+++ src/App.js
@@ -18,19 +18,19 @@ const HeroSection = () => {
       }}
     >
       <div className="max-w-xl rounded">
-        <h2 className="text-left text-xl font-bold text-black mb-4">
+        <h2 className="text-left md:text-xl text-md font-bold text-black mb-4">
           Quality cleaning at a fair price.
         </h2>
-        <h1 className="text-left text-5xl font-bold text-black leading-tight mb-6">
+        <h1 className="text-left md:text-5xl text-xl font-bold text-black leading-tight mb-6">
           Specialized, efficient,
           <br /> and thorough cleaning <br />
           services
         </h1>
-        <p className="text-xl text-left text-gray-600 mb-8">
+        <p className="md:text-xl text-md text-left text-gray-600 mb-8">
           We provide performing cleaning tasks using the least <br />
           amount of time, energy, and money.
         </p>
-        <div className="flex gap-4">
+        <div className="flex gap-4 md:text-xl text-md">
           <button className="bg-[#44acfc] text-white px-6 py-2 rounded shadow hover:bg-blue-500">
             Get Start Now
           </button>
